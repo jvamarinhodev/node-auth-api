7e4fa9225c5e9d9af2ab4b3310ebbc6acc8671fc
Hypeeer <vittorjoao18@hotmail.com>
2026-06-02 22:52:42 -0300

    feat(routers): add login router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
 
 import { postUserRegistration } from '../controllers/user/registerController.js';
+import { postLoginAuthorization } from '../controllers/user/loginController.js';
 
 const router = express.Router();
 
 router.post('/register', postUserRegistration);
+router.post('/login', postLoginAuthorization);
 export default router;
