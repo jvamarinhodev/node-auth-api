86409e1fe5af0d64813be9dda21b959ac3c3c741
Hypeeer <vittorjoao18@hotmail.com>
2026-06-02 22:41:39 -0300

    feat(login): Login route working

---
+++ src/controllers/user/loginController.js
@@ -0,0 +1,47 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { saveRefreshToken, searchEmail } from '../../database/queries/userQueries.js';
+
+export const postLoginAuthorization = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid e-mail or password field!',
+    });
+  }
+
+  const identifiedUser = await searchEmail(email);
+  console.log(identifiedUser);
+
+  if (!identifiedUser || identifiedUser.length === 0) {
+    return res.status(400).json({
+      success: false,
+      massage: 'User not found!',
+    });
+  }
+
+  const hashPassword = identifiedUser.password;
+
+  const verify = await bcrypt.compare(password, hashPassword);
+
+  if (verify === true) {
+    const accessToken = jwt.sign({ id: identifiedUser[0].id }, process.env.JWT_ACCESS_TOKEN, { expiresIn: '10m' });
+
+    const refreshToken = jwt.sign({ id: identifiedUser[0].id }, process.env.JWT_ACCESS_TOKEN, { expiresIn: '1d' });
+    // Save refreshToken together user id
+    await saveRefreshToken(refreshToken, identifiedUser[0].id);
+
+    return res.status(200).json({
+      success: true,
+      message: 'Login successful!',
+      accessToken,
+    })
+  }else if (verify === false){
+    return res.status(400).json({
+      success: false,
+      message: 'Incorrect password!'
+    })
+  }
+};
