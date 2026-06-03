ec10139a91dc53793e833d37f25d1495be43b491
Hypeeer <vittorjoao18@hotmail.com>
2026-06-02 22:48:50 -0300

    feat(database): adds user search by email and persistence of refresh token

--- src/database/queries/userQueries.js
+++ src/database/queries/userQueries.js
@@ -1,10 +1,21 @@
 import pool from '../conection.js';
 
 export const saveUser = async (name, email, passwordHash) => {
-  const result = await pool.query(
-    `INSERT INTO users (name, email, password ) VALUES ($1, $2, $3) RETURNING id`,
-    [name, email, passwordHash]
-  );
+  const result = await pool.query(`INSERT INTO users (name, email, password ) VALUES ($1, $2, $3) RETURNING id`, [
+    name,
+    email,
+    passwordHash,
+  ]);
 
   return result.rows[0].id;
+};
+
+export const searchEmail = async (email) => {
+  const result = await pool.query(`SELECT id, password FROM users WHERE email = $1`, [email]);
+  return result.rows[0];
+};
+
+export const saveRefreshToken = async (refreshToken, userID) => {
+  const result = await pool.query(`UPDATE users SET refreshToken = $1 WHERE id = $2`, [refreshToken, userID]);
+  return result;
 };
