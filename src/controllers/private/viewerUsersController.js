4ecb4bdab55aa56183ef9860888994e7eb2d1479
Hypeeer <vittorjoao18@hotmail.com>
2026-06-12 08:15:23 -0300

    refacto(status): change status code

--- src/controllers/private/viewerUsersController.js
+++ src/controllers/private/viewerUsersController.js
@@ -3,7 +3,7 @@ import { getAllUsers } from '../../database/queries/userQueries.js';
 export const getListUser = async (req, res) => {
   try {
     const listUser = await getAllUsers();
-    res.status(201).json(listUser);
+    res.status(200).json(listUser);
   } catch (err) {
     return res.status(500).json({ err: 'Server error', message: err.message });
   }
--- src/database/queries/userQueries.js
+++ src/database/queries/userQueries.js
@@ -21,6 +21,6 @@ export const saveRefreshToken = async (refreshToken, userID) => {
 };
 
 export const getAllUsers = async () => {
-  const result = await pool.query(`SELECT id, name, email, FROM users`);
-  return result;
+  const result = await pool.query(`SELECT id, name, email FROM users`);
+  return result.rows;
 };
