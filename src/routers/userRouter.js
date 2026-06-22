89090a781e644f4760aa3805bb30dc2fc8fac885
Hypeeer <vittorjoao18@hotmail.com>
2026-06-22 07:49:37 -0300

    feat(router): token revalidation router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,10 +4,12 @@ import { getListUser } from '../controllers/private/viewerUsersController.js';
 import { postUserRegistration } from '../controllers/user/registerController.js';
 import { postLoginAuthorization } from '../controllers/user/loginController.js';
 import { authorization } from '../middlewares/authorization.js';
+import { postRefreshToken } from '../controllers/user/authRefreshTokenController.js';
 
 const router = express.Router();
 
 router.post('/register', postUserRegistration);
 router.post('/login', postLoginAuthorization);
 router.get('/auth/profile/', authorization, getListUser);
+router.post('/auth/refreshtoken', postRefreshToken);
 export default router;
