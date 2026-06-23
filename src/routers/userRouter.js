97580454970301b85c4cfc12ea4549caa71b7a43
Hypeeer <vittorjoao18@hotmail.com>
2026-06-23 08:25:49 -0300

    feat(router): logout route

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,6 +5,7 @@ import { postUserRegistration } from '../controllers/user/registerController.js'
 import { postLoginAuthorization } from '../controllers/user/loginController.js';
 import { authorization } from '../middlewares/authorization.js';
 import { postRefreshToken } from '../controllers/user/authRefreshTokenController.js';
+import { postLogoutUser } from '../controllers/user/authLogout.js';
 
 const router = express.Router();
 
@@ -12,4 +13,5 @@ router.post('/register', postUserRegistration);
 router.post('/login', postLoginAuthorization);
 router.get('/auth/profile/', authorization, getListUser);
 router.post('/auth/refreshtoken', postRefreshToken);
+router.post('/auth/logout', postLogoutUser);
 export default router;
