0fbf6de4d6c930ad02860e7f483e81216993cc2e
Hypeeer <vittorjoao18@hotmail.com>
2026-07-15 21:33:45 -0300

    fix: corrects absolute URLs in handlebars templates

--- client/app.js
+++ client/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import { engine } from 'express-handlebars';
 import { fileURLToPath } from 'url';
+import { router } from './src/routers/authRouter.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,13 +14,15 @@ app.engine(
   'handlebars',
   engine({
     defaultLayout: 'main',
-    layoutsDir: './views/layouts',
-    partialsDir: './views/partials',
+    layoutsDir: path.join(__dirname, 'src/views/layouts'),
+    partialsDir: path.join(__dirname, 'src/views/partials'),
   })
 );
 
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'src/views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // *! understand
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', router);
