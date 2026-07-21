413f902666d5ab3ac32bf76d77bd45c7c739e915
Hypeeer <vittorjoao18@hotmail.com>
2026-07-21 08:28:37 -0300

    feat(client): creates router paths

--- client/src/routers/authRouter.js
+++ client/src/routers/authRouter.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { postRegister, getRegister } from '../controllers/userController.js';
+import { postRegister, getRegister } from '../controllers/registerController.js';
+import { postLogin, getLogin } from '../controllers/loginController.js';
 
 export const router = express.Router();
 
 router.post('/register', postRegister);
 router.get('/register', getRegister);
+router.post('/login', postLogin)
+router.get('/login', getLogin)
