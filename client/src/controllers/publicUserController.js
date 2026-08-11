206db3ba258062bc24f964384f91abb611714442
Hypeeer <vittorjoao18@hotmail.com>
2026-08-11 08:10:57 -0300

    feat(client): Display registered user data via the public router

--- client/src/controllers/publicUserController.js
+++ client/src/controllers/publicUserController.js
@@ -2,7 +2,11 @@ import { apiClient } from '../services/apiServices.js';
 
 export const getPublicAccess = async (req, res) => {
   try {
-    res.render('dashboard/start');
+    const response = await apiClient('/profile', {
+      method: 'GET',
+    });
+
+    res.render('dashboard/start', { response });
   } catch (error) {
     res.status(500).send(`error: ${error.message}`);
   }
